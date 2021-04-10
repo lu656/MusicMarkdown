1fc9a5f3ac6428be4dd2d0a36cedb6125ceeb3ca
rishyraj <rishyraj10@gmail.com>
2021-04-10 04:41:17 -0400

    Improved Lex Parser functionality

--- parser.js
+++ parser.js
@@ -19,7 +19,7 @@ function isInstrumentFooter(c) {
 }
 
 function isMeasure(c) {
-    return c == '|';
+    return c[0] == '|';
 }
 
 function isMeasureMeta(c) {
@@ -38,9 +38,9 @@ function isChordFooter(c) {
     return c == ']';
 }
 
-function isTie(c) {
-    return c == '-';
-}
+// function isTie(c) {
+//     return c == '-';
+// }
 
 function isWhitespace(c) {
     return /\s/.test(c);
@@ -50,65 +50,129 @@ function isNewline(c) {
     return c == "\n";
 }
 
+function getMeasureData(input) {
+    var measureData = [];
+    var c = "";
+    var i = 1;
+    var advance = function() { 
+        return c = input[++i];
+    };
+    var addToken = function (tokens, lex_obj) {
+        tokens.push(lex_obj);
+        return tokens;
+    };
+
+    chords = [];
+    isInChord = false;
+
+    while (i < input.length) {
+        // console.log(c);
+        c += input[i];
+        console.log(c);
+        if (isWhitespace(c)) {
+            // advance();
+            c = "";
+            console.log(c);
+            i++;
+        } else if (isMeasureMeta(c)) {
+            measureData = addToken(measureData,{type:"measureMeta",value:c});
+            advance();
+            console.log(c);
+        } else if (isNote(c)) {
+            if (!isInChord) {
+                measureData = addToken(measureData,{type:"note",value:c});
+                // advance();
+                c = "";
+                i++;
+                console.log(c);
+            } else {
+                chords = addToken(chords,{type:"note",value:/\([ABCDEF]{1}[b#]{0,1},\s*[0-9]+,\s*[0-9]+\)/.exec(c)[0]});
+                // advance();
+                c = "";
+                i++;
+                console.log(c);
+
+            }
+        } else if (isChordHeader(c)) {
+            isInChord = true;
+            chords = [];
+            c = "";
+            i++;
+        } else if (isChordFooter(c)) {
+            isInChord = false;
+            measureData = addToken(measureData,{type:"chord",value:chords});
+            c = "";
+            i++;
+        } else {
+            i++;
+        }
+    }
+
+
+    return measureData;
+
+} 
+
 function lex(input) {
     var tokens = [];
-    var c = 0;
+    var c = "";
     var i = 0;
     var advance = function() { 
         return c = input[++i];
     };
 
-    var addToken = function (type, value) {
-        tokens.push({
-            type: type,
-            value: value
-        });
+    var addToken = function (tokens, lex_obj) {
+        tokens.push(lex_obj);
+        return tokens;
     };
 
-    while (i < input.length) {
-        c = input[i];
-        if (isWhitespace(c)) {
-            advance();
-        } else if (isNewline(c)) {
-            addToken("newline",c);
-            advance();
-        }else if (isTitle(c)) {
-            addToken("title",c);
-            advance();
-        } else if (isAuthor(c)) {
-            addToken("author",c);
-            advance();
-        } else if (isInstrumentHeader(c)) {
-            addToken("instrumentHeader",c);
-            advance();
-        } else if (isInstrumentFooter(c)) {
-            addToken("instrumentFooter",c);
-            advance();
-        } else if (isMeasure(c)) {
-            addToken("measure",c);
-            advance();
-        } else if (isMeasureMeta(c)) {
-            addToken("measureMeta",c);
-            advance();
-        } else if (isNote(c)) {
-            addToken("note",c);
-            advance();
-        } else if (isChordHeader(c)) {
-            addToken("chordHeader",c);
-            advance();
-        } else if (isChordFooter(c)) {
-            addToken("chordFooter",c);
-            advance();
-        } else if (isTie(c)) {
-            addToken("tie",c);
-            advance();
+    input_split = input.split("\n");
+    measures = [];
+    measureData = [];
+    staveNum = 1;
+    measureNum = 0;
+    instrument = "";
+    isInInstrument = false;
+
+    for (var i = 0; i < input_split.length; i++) {
+        line = input_split[i].trim();
+        if (isTitle(line)) {
+            tokens = addToken(tokens,{type:"title",value:line});
+        } else if (isAuthor(line)) {
+            tokens = addToken(tokens,{type:"author",value:line});
         } else {
-            throw "Syntax Error";
+            if (isInstrumentHeader(line)) {
+                isInInstrument = true;
+                measures = [];
+                measureNum = 1;
+                staveNum = 0;
+                instrument = /[a-zA-Z]+/.exec(line)[0];
+            } else {
+                if (isMeasure(line)) {
+                    staveNum++;
+                    measureData = getMeasureData(line);
+                    measures = addToken(measures,{type:"measure",value:measureData,staveNum:staveNum,measureNum:measureNum});
+                } else if (line=="" && isInInstrument == true) {
+                    console.log("hit");
+                    staveNum = 0;
+                    measureNum++;
+                } else if (isInstrumentFooter(line)) {
+                    isInInstrument = false;
+                    tokens = addToken(tokens,{type:"instrument",instrument:instrument,value:measures});
+                }
+            }
         }
     }
 
-    addToken("(end)");
+    return tokens;
+
+}
+
+function test_lex() {
+    text = document.getElementById("music_markdown_textarea").value;
+    tokens = console.log(lex(text));
     return tokens;
 }
 
 // console.log(isMeasureMeta("$T, 4/4, Am$"));
+
