3577eb149c45a2eb9af620e14e15ec183e80d009
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-04-10 17:46:47 -0400

    Added modal

--- frontend/src/ExamplesNavbar.js
+++ frontend/src/ExamplesNavbar.js
@@ -1,35 +1,52 @@
-/*!
-
-=========================================================
-* BLK Design System React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/blk-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from 'react';
 import { Link } from 'react-router-dom';
-// reactstrap components
-import { Button, Collapse, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container, Row, Col, UncontrolledTooltip } from 'reactstrap';
+import classnames from 'classnames';
+import {
+  Button,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+  Modal,
+  FormGroup,
+  UncontrolledPopover,
+  PopoverBody,
+  PopoverHeader,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Label,
+  UncontrolledCarousel,
+} from 'reactstrap';
 
 export default function ExamplesNavbar() {
   const [collapseOpen, setCollapseOpen] = React.useState(false);
   const [collapseOut, setCollapseOut] = React.useState('');
   const [color, setColor] = React.useState('navbar-transparent');
+  const [signInFormModal, setsignInFormModal] = React.useState(false);
+  const [signInEmailFocus, setSignInEmailFocus] = React.useState(false);
+  const [signInPasswordFocus, setSignInPasswordFocus] = React.useState(false);
+
+  const [createAccountModal, setCreateAccountModal] = React.useState(false);
+  const [createAccountEmailFocus, setCreateAccountEmailFocus] = React.useState(false);
+  const [createAccountPasswordFocus, setCreateAccountPasswordFocus] = React.useState(false);
+  const [createAccountRetypePasswordFocus, setCreateAccountRetypePasswordFocus] = React.useState(false);
+
   React.useEffect(() => {
     window.addEventListener('scroll', changeColor);
     return function cleanup() {
       window.removeEventListener('scroll', changeColor);
     };
   }, []);
+
   const changeColor = () => {
     if (document.documentElement.scrollTop > 99 || document.body.scrollTop > 99) {
       setColor('bg-info');
@@ -37,16 +54,20 @@ export default function ExamplesNavbar() {
       setColor('navbar-transparent');
     }
   };
+
   const toggleCollapse = () => {
     document.documentElement.classList.toggle('nav-open');
     setCollapseOpen(!collapseOpen);
   };
+
   const onCollapseExiting = () => {
     setCollapseOut('collapsing-out');
   };
+
   const onCollapseExited = () => {
     setCollapseOut('');
   };
+
   return (
     <Navbar className={'fixed-top ' + color} color-on-scroll='100' expand='lg'>
       <Container>
@@ -91,9 +112,162 @@ export default function ExamplesNavbar() {
                 <p className='d-lg-none d-xl-none'>GitHub</p>
               </NavLink>
             </NavItem>
+            <NavItem>
+              <Button
+                className='nav-link d-none d-lg-block'
+                color='primary'
+                onClick={() => {
+                  setCreateAccountModal(true);
+                }}>
+                <i className='tim-icons icon-spaceship' /> Create an Account
+              </Button>
+            </NavItem>
+            <NavItem>
+              <Button
+                className='nav-link d-none d-lg-block'
+                color='default'
+                onClick={() => {
+                  setsignInFormModal(true);
+                }}>
+                <i className='tim-icons icon-single-02' /> Sign in
+              </Button>
+            </NavItem>
           </Nav>
         </Collapse>
       </Container>
+      <Modal modalClassName='modal-black' isOpen={signInFormModal} toggle={() => setsignInFormModal(false)}>
+        <div className='modal-header justify-content-center'>
+          <button className='close' onClick={() => setsignInFormModal(false)}>
+            <i className='tim-icons icon-simple-remove' />
+          </button>
+          <h4 className='title title-up'>Sign in with email</h4>
+        </div>
+        <div className='modal-body'>
+          <Form role='form'>
+            <FormGroup className='mb-3'>
+              <InputGroup
+                className={classnames('input-group-alternative', {
+                  'input-group-focus': signInEmailFocus,
+                })}>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='tim-icons icon-email-85' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder='Email' type='email' onFocus={(e) => setSignInEmailFocus(true)} onBlur={(e) => setSignInEmailFocus(false)} />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup
+                className={classnames('input-group-alternative', {
+                  'input-group-focus': signInPasswordFocus,
+                })}>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='tim-icons icon-key-25' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  placeholder='Password'
+                  type='password'
+                  onFocus={(e) => setSignInPasswordFocus(true)}
+                  onBlur={(e) => setSignInPasswordFocus(false)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup check className='mt-3'>
+              <Label check>
+                <Input defaultChecked type='checkbox' />
+                <span className='form-check-sign' />
+                Remember me!
+              </Label>
+            </FormGroup>
+            <div className='text-center'>
+              <Button className='my-4' color='primary' type='button'>
+                Sign in
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Modal>
+      <Modal modalClassName='modal-black' isOpen={createAccountModal} toggle={() => setCreateAccountModal(false)}>
+        <div className='modal-header justify-content-center'>
+          <button className='close' onClick={() => setCreateAccountModal(false)}>
+            <i className='tim-icons icon-simple-remove' />
+          </button>
+          <h4 className='title title-up'>Create account with email</h4>
+        </div>
+        <div className='modal-body'>
+          <Form role='form'>
+            <FormGroup className='mb-3'>
+              <InputGroup
+                className={classnames('input-group-alternative', {
+                  'input-group-focus': createAccountEmailFocus,
+                })}>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='tim-icons icon-email-85' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  placeholder='Email'
+                  type='email'
+                  onFocus={(e) => setCreateAccountEmailFocus(true)}
+                  onBlur={(e) => setCreateAccountEmailFocus(false)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup className='has-danger'>
+              <InputGroup
+                className={classnames('input-group-alternative', {
+                  'input-group-focus': createAccountPasswordFocus,
+                })}>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='tim-icons icon-key-25' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  placeholder='Password'
+                  type='password'
+                  onFocus={(e) => setCreateAccountPasswordFocus(true)}
+                  onBlur={(e) => setCreateAccountPasswordFocus(false)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup className='has-danger'>
+              <InputGroup
+                className={classnames('input-group-alternative', {
+                  'input-group-focus': createAccountRetypePasswordFocus,
+                })}>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='tim-icons icon-key-25' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  placeholder='Retype Password'
+                  type='password'
+                  onFocus={(e) => setCreateAccountRetypePasswordFocus(true)}
+                  onBlur={(e) => setCreateAccountRetypePasswordFocus(false)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup check className='mt-3'>
+              <Label check>
+                <Input defaultChecked type='checkbox' />
+                <span className='form-check-sign' />
+                Remember me!
+              </Label>
+            </FormGroup>
+            <div className='text-center'>
+              <Button className='my-4' color='primary' type='button'>
+                Sign in
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Modal>
     </Navbar>
   );
 }
