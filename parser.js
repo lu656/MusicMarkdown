f5bba61cc41c354ae4f23f4d93f340a8d478e560
lu656 <lu656@purdue.edu>
2021-04-10 04:10:27 -0400

    moved parsing to evaluate. started converting lexed values to xml

--- parser.js
+++ parser.js
@@ -113,38 +113,3 @@ function lex(input) {
 
 
 
-var parse = function(tokens) {
-    var parseTree = [];
-    var types = ['title', 'author', 'instrumentHeader', 'instrumentFooter', 'measure', 'measureMeta', 'note', 'tie', 'chord', 'newline'];
-    
-    var symbols = {};
-    var symbol = function (id, type, val) {
-        var sym = symbols[id] || {};
-        symbols[id] = {
-            type: sym.type || type,
-            val: sym.value || val
-        };
-    };
-    var getToken = function (token) {
-        var sym = Object.create(symbols[token.type]);
-        sym.type = token.type;
-        sym.value = token.value;
-        return sym;
-    }
-
-    var i = 0;
-    var token = function () {
-        return getToken(tokens[i]);
-    }
-    
-    var advance = function () {
-        i++;
-        return token();
-    }
-    while (token().type !== "(end)") {
-
-        advance()
-    }
-    
-    return parseTree;
-}
