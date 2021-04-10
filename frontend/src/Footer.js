1e4bbbb43acd2d51cb34e4e63a9bd048ea29f79e
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-04-10 16:13:03 -0400

    Added frontend

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
       <Container>
         <Row>
           <Col md="3">
-            <h1 className="title">BLK•</h1>
+            <h1 className="title">MMD • Music Markdown</h1>
           </Col>
           <Col md="3">
             <Nav>
---
+++ frontend/src/Syntax.js
@@ -0,0 +1,210 @@
+import 'brace/mode/java';
+
+export class CustomHighlightRules extends window.ace.acequire('ace/mode/text_highlight_rules').TextHighlightRules {
+  constructor() {
+    super();
+    this.$rules = {
+      start: [
+        {
+          token: 'keyword.operator',
+          regex: '(\\{)',
+          push: 'main__1',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\[)',
+          push: 'main__2',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\()',
+          push: 'main__3',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(,|\\:)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          push: 'main__4',
+        },
+        {
+          token: 'storage.type',
+          regex: '((?:\\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\\.[0-9]*)(?:(?:e|E)(?:\\+|\\-)[0-9]+)?)?)',
+        },
+        {
+          defaultToken: 'text',
+        },
+      ],
+      main__1: [
+        {
+          token: 'keyword.operator',
+          regex: '(\\})',
+          next: 'pop',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\{)',
+          push: 'main__1',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\[)',
+          push: 'main__2',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\()',
+          push: 'main__3',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(,|\\:)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          push: 'main__4',
+        },
+        {
+          token: 'storage.type',
+          regex: '((?:\\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\\.[0-9]*)(?:(?:e|E)(?:\\+|\\-)[0-9]+)?)?)',
+        },
+        {
+          defaultToken: 'text',
+        },
+      ],
+      main__2: [
+        {
+          token: 'keyword.operator',
+          regex: '(\\])',
+          next: 'pop',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\{)',
+          push: 'main__1',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\[)',
+          push: 'main__2',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\()',
+          push: 'main__3',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(,|\\:)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          push: 'main__4',
+        },
+        {
+          token: 'storage.type',
+          regex: '((?:\\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\\.[0-9]*)(?:(?:e|E)(?:\\+|\\-)[0-9]+)?)?)',
+        },
+        {
+          defaultToken: 'text',
+        },
+      ],
+      main__3: [
+        {
+          token: 'keyword.operator',
+          regex: '(\\))',
+          next: 'pop',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\{)',
+          push: 'main__1',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\[)',
+          push: 'main__2',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\()',
+          push: 'main__3',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(,|\\:)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          push: 'main__4',
+        },
+        {
+          token: 'storage.type',
+          regex: '((?:\\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\\.[0-9]*)(?:(?:e|E)(?:\\+|\\-)[0-9]+)?)?)',
+        },
+        {
+          defaultToken: 'text',
+        },
+      ],
+      main__4: [
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          next: 'pop',
+        },
+        {
+          token: 'constant.character.escape',
+          regex: '(\\\\(?:\\\\|%))',
+        },
+        {
+          token: 'string.unquoted',
+          regex: '([^%\\\\]+)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\{)',
+          push: 'main__1',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\[)',
+          push: 'main__2',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(\\()',
+          push: 'main__3',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(,|\\:)',
+        },
+        {
+          token: 'keyword.operator',
+          regex: '(%)',
+          push: 'main__4',
+        },
+        {
+          token: 'storage.type',
+          regex: '((?:\\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\\.[0-9]*)(?:(?:e|E)(?:\\+|\\-)[0-9]+)?)?)',
+        },
+        {
+          defaultToken: 'text',
+        },
+      ],
+    };
+    this.normalizeRules();
+  }
+}
+
+export default class CustomRule extends window.ace.acequire('ace/mode/java').Mode {
+  constructor() {
+    super();
+    this.HighlightRules = CustomHighlightRules;
+  }
+}
