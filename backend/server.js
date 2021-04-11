529e529ca2d586274988a4f8a54aab6e418f73ca
engelsa <engelsa@purdue.edu>
2021-04-10 22:42:31 -0400

    Added a little curly brace :)

--- backend/server.js
+++ backend/server.js
@@ -38,58 +38,60 @@ const db = new Firestore({
 });
 
 async function handleLogin(req, res) {
-	// req.on('data', async)
-	// const docRef = db.collection('users').doc('alovelace');
+    // req.on('data', async)
+    // const docRef = db.collection('users').doc('alovelace');
 
-	// req.on('data', async function(data) {
-	// 	// console.log(JSON.parse(data));
-	// 	let body = JSON.parse(data);
-	// 	console.log(body);
+    // req.on('data', async function(data) {
+    // 	// console.log(JSON.parse(data));
+    // 	let body = JSON.parse(data);
+    // 	console.log(body);
 
-	// 	// await docRef.set(JSON.parse(data));
-	// })
-	// const queryObject = url.parse(req.url, true).query;
-	// console.log(queryObject);
-	// console.log((JSON.stringify(req.headers)))
+    // 	// await docRef.set(JSON.parse(data));
+    // })
+    // const queryObject = url.parse(req.url, true).query;
+    // console.log(queryObject);
+    // console.log((JSON.stringify(req.headers)))
 
-	let body = JSON.parse(JSON.stringify(req.headers))
-	let email = body['email'];
-	let password = body['password'];
+    let body = JSON.parse(JSON.stringify(req.headers))
+    let email = body['email'];
+    let password = body['password'];
 
-	let docRef = db.collection("users").doc(email);
+    let docRef = db.collection("users").doc(email);
 
-	var getOptions = {
-		source: 'server'
-	};
+    var getOptions = {
+        source: 'server'
+    };
 
-	docRef.get(getOptions).then((doc) => {
-		let data = doc.data();
-		console.log('got data:', data.name);
-		if (data.password === password) {
-			res.writeHead(200);
-			res.end('Username and password found');
-		} else {
-			res.writeHead(401);
-			res.end('Invalid Username and Password');
-		}
-		return;
-	}).catch((error) => {
-		console.log('failed to get document', error);
-		res.writeHead(401);
-		res.end('Invalid Username and Password');
-		return;
-	});
-	// console.log(email);
-	// console.log(password)
-	// console.log(body)
+    docRef.get(getOptions).then((doc) => {
+        let data = doc.data();
+        console.log('got data:', data.name);
+        if (data.password === password) {
+            res.writeHead(200);
+            res.end('Username and password found');
+        } else {
+            res.writeHead(401);
+            res.end('Invalid Username and Password');
+        }
+        return;
+    }).catch((error) => {
+        console.log('failed to get document', error);
+        res.writeHead(401);
+        res.end('Invalid Username and Password');
+        return;
+    });
+    // console.log(email);
+    // console.log(password)
+    // console.log(body)
 
-	// const snapshot = await db.collection('users').get();
-	// snapshot.forEach((doc) => {
-	// 	console.log(doc.id, '=>', doc.data());
-	// });
+    // const snapshot = await db.collection('users').get();
+    // snapshot.forEach((doc) => {
+    // 	console.log(doc.id, '=>', doc.data());
+    // });
 
-	// res.writeHead(200);
-	// res.end('Data saved!');
+    // res.writeHead(200);
+    // res.end('Data saved!');
+}
+
 function getSaveDataJSON(data) {
     let rawJSON;
     try {
